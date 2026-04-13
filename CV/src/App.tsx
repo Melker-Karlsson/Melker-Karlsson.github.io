8c36b43398ca1d678411648b0aaa76ab840ecf20
Melker-Karlsson <melker.karlsson041008@gmail.com>
2026-04-13 15:52:30 +0200

    added profile component

--- CV/src/App.tsx
+++ CV/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import './components/Profile'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div id='cv'>
+      <Profile/>
+      <br />
+    </div>
   )
 }
 
